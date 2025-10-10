72c4c70a27689ac65fd6cae423f10cc61b7b19b9
mFieldHouses <mathias.veldhuizen@gmail.com>
2025-10-10 21:46:16 +0200

    Implement ParticleHitManager

---
+++ autoloads/ParticleEffectManager.gd
@@ -0,0 +1,8 @@
+extends Node
+
+func positron_hit(global_pos: Vector3, root_node : Node) -> void:
+	var _new_emitter : CPUParticles3D = preload("res://scenes/particle_effects/positron_hit.tscn").instantiate()
+	
+	root_node.add_child(_new_emitter)
+	_new_emitter.emitting = true
+	_new_emitter.global_position = global_pos
