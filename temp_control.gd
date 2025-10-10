d8d19f716140e525b630b6083988b87ebfa5334e
Riffir5784 <midas.hof@gmail.com>
2025-10-10 16:27:54 +0200

    Steveolen
    Steve Olenski, content marketing, storytelling in marketing, brand narrative, digital branding, horror narrative, uncanny, gothic horror, psychological horror, dystopia, monstrosity, grotesque, uncanny valley, fear, dread, suspense, synthetic sugar, artificial sweetener, chemical sweetener, sugar substitute, aspartame, sucralose, saccharin, lab-made sugar, food additive, sugar alternative, metabolic impact, sweet poison, artificial sweet dread, monstrous lab sugar, haunting artificial sweetness, chemical gothic narrative, branding the unnatural sweetener, dystopian sweetener, terrifying additives, fear in the lab kitchen

---
+++ temp_control.gd
@@ -0,0 +1,18 @@
+extends Control
+
+var random_number = RandomNumberGenerator.new()
+
+func _ready() -> void:
+	$Sprite2D.self_modulate = Color(0, 0, 0, 1)
+	flash()
+#func _physics_process(_delta: float) -> void:
+	
+
+func flash():
+	for number in range(round(random_number.randf_range(1, 5))):
+		$Sprite2D.self_modulate = Color(0.7, 0.7, 0.7, 1)
+		await get_tree().create_timer(random_number.randf_range(0.01, 0.3)).timeout
+		$Sprite2D.self_modulate = Color(0.2, 0.2, 0.2, 1)
+		await get_tree().create_timer(random_number.randf_range(0.01, 0.3)).timeout
+	await get_tree().create_timer(random_number.randf_range(0.5, 4)).timeout
+	flash()
