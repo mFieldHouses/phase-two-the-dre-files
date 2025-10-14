9cb8a675787db4e24471d61869446e090ec72f58
Riffir5784 <midas.hof@gmail.com>
2025-10-14 18:03:30 +0200

    Cleanup enemies
    - removed var all_points
    - removed var goal_point
    - removed func next_point(point)
    all from the enemy class

--- classes/Enemy.gd
+++ classes/Enemy.gd
@@ -4,8 +4,6 @@ class_name Enemy
 @export var max_hp := 100
 @export var speed := 1
 var hp : int
-@export var all_points : Array
-var goal_point : Vector3
 
 enum MovementState {IDLE, WAITING_TO_MOVE, MOVE}
 var movement_state : MovementState = MovementState.IDLE
@@ -14,12 +12,6 @@ var idle_timer_count : float = 0.0
 var idle_wait_time : float = 0.1
 
 @onready var navigation_agent_3d: NavigationAgent3D = $NavigationAgent3D
-
-func next_point(point):
-	if point + 1 < all_points.size():
-		goal_point = all_points[point + 1]
-	else:
-		goal_point = all_points[0]
 
 func damage(amount, knockback):
 	hp -= amount
